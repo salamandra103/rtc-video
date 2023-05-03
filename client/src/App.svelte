39f5a870c4d309586e112c8c96f384a9aca592e7
Владимир Скрепков <you@example.com>
2023-05-04 01:20:26 +0300

    init svelte client

---
+++ client/src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <div>RTC Video chat client</div>
+
+  <!-- <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p> -->
+</main>
+
+<style>
+</style>
